20bdc3e758810dbae4ea0012600015321f2a1785
Matheus Raz <mrol@cin.ufpe.br>
2018-07-20 15:48:47 -0300

    pre-finalização do front

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import Teams from './Teams'
+import TeamsMatches from './TeamsMatches'
+import main from './css/App.css'
 
 export default class App extends React.Component {
   render() {
     return(
       <div class="main">
         <h1>Copa do Mundo!</h1>
-        <Teams title='*Visualize os placares das seleções na copa*'/>
+        <TeamsMatches title='*Visualize os placares das seleções na copa*'/>
       </div>
     )
   }
---
+++ src/components/TeamsMatches.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { Accordion, AccordionItem } from 'react-sanfona';
+import Score from './Score'
+
+export default class TeamsMatches extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            teams: [],
+            teamChoice: '',
+            teamMatches: []
+        };
+        this.onClickTeam = this.onClickTeam.bind(this);
+    }
+
+    onClickTeam(item) {
+      this.setState({teamChoice: item});
+      fetch(`http://localhost:8080/matches/${item}`)
+            .then(resultsMatch => {
+                return resultsMatch.json();
+            }).then(dataMatch => {
+                //console.log(dataMatch)
+                this.setState({teamMatches: dataMatch});
+                console.log(this.state.teamMatches);
+            });
+    }
+
+    componentDidMount() {
+
+        fetch(`http://localhost:8080/teams`)
+        .then(results => {
+            return results.json();
+        }).then(data => {
+            //console.log(data);
+            this.setState({teams: data})
+            console.log('Times:',this.state.teams);
+        })
+    }
+
+    render() {
+        return (
+        <div>
+            <h2>{this.props.title}</h2>
+          <Accordion>
+            {this.state.teams.map((item, index) => {
+              return (
+                <AccordionItem key={index} title={`${item.country}`} onExpand={(e) => this.onClickTeam(item.country, e)}>
+                <div>
+                    {this.state.teamMatches.map((data, ind) => {
+                        return(
+                            <div key={ind}>
+                                <Score text={`(${data.home.code})${data.home.country} ${data.home.goals} X ${data.away.goals} ${data.away.country}(${data.away.code})`}/>
+                            </div>
+                        )
+                    })}
+                </div>
+                </AccordionItem>
+              );
+            })}
+          </Accordion>
+          </div>
+        );
+      }
+}
